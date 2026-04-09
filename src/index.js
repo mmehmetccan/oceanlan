56912190f85c550181f0fd86d95d9a46ad3b1b33
Mehmet Can <mehmet@example.com>
2026-04-09 14:54:36 +0300

    1.0.0.9

--- src/index.js
+++ src/index.js
@@ -47,10 +47,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cors());
 app.use(helmet());
-app.use(cors({
-    origin: ["http://localhost:5173", "app://.", "your-production-url.com"],
-    credentials: true
-}));
+
 
 // 3. HTTP Parameter Pollution önleme (Aynı parametreden 2 tane gelmesini engeller)
 app.use(hpp());
